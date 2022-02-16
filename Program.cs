46e5eb8f1b88f7be94382885b7608b9f4b0a4096
Hector Peeters <hector.peeters@gmail.com>
2022-02-16 14:29:29 +0100

    Add basic parsing types

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public struct ParseResult<T>
+{
+    bool Success;
+    int Offset;
+    string[] Expected;
+    bool Fatal;
+}
+
+public interface Parser<T>
+{
+    public ParseResult<T> Parse(string input, int offset);
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+    }
+}
