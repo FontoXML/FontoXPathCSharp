f21b6456eec5c6c40e95d15c1c6fcc238d528ba5
Marijn Timmerije <M.Timmerije@student.tudelft.nl>
2022-03-23 17:29:50 +0100

    Got a bit further with external evaluation API

--- FontoXPathCSharp/CompileAstToExpression.cs
+++ FontoXPathCSharp/CompileAstToExpression.cs
@@ -68,7 +68,7 @@ public static class CompileAstToExpression
         if (functionName == null)
             throw new InvalidDataException(ast.Name);
 
-        return new FunctionCall(new NamedFunctionRef(functionName.GetQName(), null));
+        return new FunctionCall(new NamedFunctionRef(functionName.GetQName(), 0));
     }
 
     public static AbstractExpression CompileAst(Ast ast)
