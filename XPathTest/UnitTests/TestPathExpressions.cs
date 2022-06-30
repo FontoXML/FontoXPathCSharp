145bb7ed2e15410e343aa326a2f135cfd5c7d1e0
Hector Peeters <hector.peeters@gmail.com>
2022-06-30 16:28:29 +0200

    Add few simple path expression tests

---
+++ XPathTest/UnitTests/TestPathExpressions.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using FontoXPathCSharp;
+using FontoXPathCSharp.Types;
+using Xunit;
+
+namespace XPathTest.UnitTests;
+
+public class TestPathExpressions
+{
+    private const string TestXml = @"<xml>
+        <herp>Herp</herp>
+        <derp id=""durp"">derp</derp>
+        <hurr durr=""durrdurrdurr"">durrrrrr</hurr>
+    </xml>";
+
+    private static readonly XmlDocument Document;
+
+    static TestPathExpressions()
+    {
+        Document = new XmlDocument();
+        Document.LoadXml(TestXml);
+    }
+
+    private static T EvalQuery<T>(string query)
+    {
+        var results = Evaluate.EvaluateXPath<T, string>(query, Document, null,
+            new Dictionary<string, IExternalValue>(), new Options());
+        return results;
+    }
+
+    private static IEnumerable<XmlNode> EvalQueryNodes(string query) =>
+        EvalQuery<IEnumerable<XmlNode>>(query);
+
+    private static string EvalQueryString(string query) =>
+        EvalQuery<string>(query);
+
+    [Fact]
+    public void SimpleAbsolutePath() =>
+        Assert.Single(EvalQueryNodes("/xml/herp"));
+
+    [Fact]
+    public void SimpleRelativePath() =>
+        Assert.Single(EvalQueryNodes("xml/herp"));
+
+    [Fact]
+    public void RelativePathEmpty() =>
+        Assert.Empty(EvalQueryNodes("xml/horp"));
+    
+    [Fact]
+    public void AbsolutePathEmpty() =>
+        Assert.Empty(EvalQueryNodes("/xml/horp"));
+    
+    [Fact]
+    public void SimpleAttribute() =>
+        Assert.Equal("durp", EvalQueryString("/xml/derp/@id"));
+}
