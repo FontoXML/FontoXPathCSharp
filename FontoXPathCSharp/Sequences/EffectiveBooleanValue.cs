374a81fee919693db93c426d36f5c18be442608d
Hector Peeters <hector.peeters@gmail.com>
2022-06-30 16:34:08 +0200

    Move EffectiveBooleanValue into FontoXPathCSharp.Sequences namespace

--- FontoXPathCSharp/Sequences/EffectiveBooleanValue.cs
+++ FontoXPathCSharp/Sequences/EffectiveBooleanValue.cs
@@ -1,13 +1,13 @@
-
 using FontoXPathCSharp.Value;
 using FontoXPathCSharp.Value.Types;
 using ValueType = FontoXPathCSharp.Value.Types.ValueType;
+
+namespace FontoXPathCSharp.Sequences;
 
 static class EffectiveBooleanValue
 {
     public static bool Compute(AbstractValue value)
     {
-
         if (SubtypeUtils.IsSubtypeOf(value.GetValueType(), ValueType.Node))
         {
             return true;
@@ -17,10 +17,12 @@ static class EffectiveBooleanValue
         {
             return value.GetAs<StringValue>(ValueType.XsString).Value.Length > 0;
         }
+
         if (SubtypeUtils.IsSubtypeOf(value.GetValueType(), ValueType.XsAnyUri))
         {
             return value.GetAs<StringValue>(ValueType.XsAnyUri).Value.Length > 0;
         }
+
         if (SubtypeUtils.IsSubtypeOf(value.GetValueType(), ValueType.XsUntypedAtomic))
         {
             return value.GetAs<StringValue>(ValueType.XsUntypedAtomic).Value.Length > 0;
