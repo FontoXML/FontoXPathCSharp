31ce91c39fd13d148e2d0237a1b1ea3665349937
Marijn <m.timmerije@student.tudelft.nl>
2022-07-08 16:24:11 +0200

    Add exists function.

--- FontoXPathCSharp/Functions/BuiltInFunctionsSequences.cs
+++ FontoXPathCSharp/Functions/BuiltInFunctionsSequences.cs
@@ -31,6 +31,9 @@ public static class BuiltInFunctionsSequences
         return arg;
     };
 
+    private static readonly FunctionDefinitionType<ISequence> FnExists = (_, _, _, args) => 
+        SequenceFactory.CreateFromValue(new BooleanValue(!args[0].IsEmpty()));
+
     private static readonly FunctionDefinitionType<ISequence> FnEmpty = (_, _, _, args) =>
         SequenceFactory.CreateFromValue(new BooleanValue(args[0].IsEmpty()));
 
@@ -46,6 +49,10 @@ public static class BuiltInFunctionsSequences
             new SequenceType(ValueType.Item, SequenceMultiplicity.ZeroOrOne)),
         new(new[] {new ParameterType(ValueType.Item, SequenceMultiplicity.ZeroOrMore)},
             FnEmpty, "empty", BuiltInUri.FUNCTIONS_NAMESPACE_URI.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsBoolean, SequenceMultiplicity.ExactlyOne)),
+        new(new[] {new ParameterType(ValueType.Item, SequenceMultiplicity.ZeroOrMore)},
+            FnExists, "exists", BuiltInUri.FUNCTIONS_NAMESPACE_URI.GetBuiltinNamespaceUri(),
             new SequenceType(ValueType.XsBoolean, SequenceMultiplicity.ExactlyOne))
+        
     };
 }
