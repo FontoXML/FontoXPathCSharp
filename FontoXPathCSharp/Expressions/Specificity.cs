3c228b68b0638780524d5eb84493afcb7bb98040
M.T <mtesseracttech@gmail.com>
2022-07-13 12:32:27 +0200

    Add specificity

---
+++ FontoXPathCSharp/Expressions/Specificity.cs
@@ -0,0 +1,59 @@
+namespace FontoXPathCSharp.Expressions;
+
+public enum SpecificityKind
+{
+    Attribute,
+    External,
+    NodeName,
+    NodeType,
+    Universal
+}
+
+public class Specificity : IComparable<Specificity>
+{
+    private static SpecificityKind[] _specificityDimensions = Enum.GetValues<SpecificityKind>();
+
+    private Dictionary<SpecificityKind, int> _counts;
+
+    public Specificity(Dictionary<SpecificityKind, int> countsByKind)
+    {
+        _counts = _specificityDimensions
+            .Select(specificityKind =>
+                (specificityKind, countsByKind.ContainsKey(specificityKind) ? countsByKind[specificityKind] : 0))
+            .ToDictionary(e => e.Item1, e => e.Item2);
+
+        //You can't pass in invalid specificity kinds with this implementation, so the check after this was not needed.
+    }
+
+    public Specificity Add(Specificity other)
+    {
+        var sum = _specificityDimensions.Aggregate(new Dictionary<SpecificityKind, int>(),
+            (countsByKind, specificityKind) =>
+            {
+                countsByKind[specificityKind] = _counts[specificityKind] + other._counts[specificityKind];
+                return countsByKind;
+            });
+
+        return new Specificity(sum);
+    }
+
+
+    public int CompareTo(Specificity? other)
+    {
+        if (other == null) return 1;
+        foreach (var specificityDim in _specificityDimensions)
+        {
+            if (other._counts[specificityDim] < _counts[specificityDim])
+            {
+                return 1;
+            }
+
+            if (other._counts[specificityDim] > _counts[specificityDim])
+            {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+}
