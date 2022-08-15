d33c3268573ba91e11613fb68f838e19875a702d
M.T <mtesseracttech@gmail.com>
2022-08-15 15:38:05 +0200

    Fix Node Sorting

--- FontoXPathCSharp/Expressions/DataTypes/DocumentOrderUtils.cs
+++ FontoXPathCSharp/Expressions/DataTypes/DocumentOrderUtils.cs
@@ -1,23 +1,58 @@
 using FontoXPathCSharp.DomFacade;
 using FontoXPathCSharp.Value;
 using FontoXPathCSharp.Value.Types;
-using ValueType = System.ValueType;
+using ValueType = FontoXPathCSharp.Value.Types.ValueType;
 
 namespace FontoXPathCSharp.Expressions.DataTypes;
 
 public class DocumentOrderUtils<TNode> where TNode : notnull
 {
+    private static List<T> Merge<T>(List<T> leftArray, List<T> rightArray, Comparison<T> comparer)
+    {
+        var sorted = new List<T>();
+
+        while (leftArray.Count > 0 && rightArray.Count > 0)
+        {
+            if (comparer(leftArray[0], rightArray[0]) < 0)
+            {
+                sorted.Add(leftArray[0]);
+                leftArray.RemoveAt(0);
+            }
+            else
+            {
+                sorted.Add(rightArray[0]);
+                rightArray.RemoveAt(0);
+            }
+        }
+
+        return sorted.Concat(leftArray.Concat(rightArray)).ToList();
+    }
+
+    public static List<T> MergeSort<T>(List<T> array, Comparison<T> comparer)
+    {
+        if (array.Count <= 1) return array;
+
+        var mid = (int)Math.Floor(array.Count / 2.0);
+        var left = MergeSort(array.Take(mid).ToList(), comparer);
+        var right = MergeSort(array.Skip(mid).ToList(), comparer);
+
+        return Merge(left, right, comparer);
+    }
+
     public static List<NodeValue<TNode>> SortNodeValues(DomFacade<TNode> domFacade, List<NodeValue<TNode>> nodeValues)
     {
-        nodeValues.Sort((node1, node2) => CompareNodePositionsWithTieBreaker(domFacade, node1, node2));
-        return nodeValues.DistinctBy(n => n.Value).ToList();
+        return MergeSort(nodeValues, (node1, node2) => CompareNodePositionsWithTieBreaker(domFacade, node1, node2))
+            .DistinctBy(n => n.Value)
+            .ToList();
+        // nodeValues.Sort((node1, node2) => CompareNodePositionsWithTieBreaker(domFacade, node1, node2));
+        // return nodeValues.DistinctBy(n => n.Value).ToList();
     }
 
     private static int CompareNodePositionsWithTieBreaker(DomFacade<TNode> domFacade, NodeValue<TNode> node1,
         NodeValue<TNode> node2)
     {
-        var isNode1SubtypeOfAttribute = node1.GetValueType().IsSubtypeOf(Value.Types.ValueType.Attribute);
-        var isNode2SubtypeOfAttribute = node2.GetValueType().IsSubtypeOf(Value.Types.ValueType.Attribute);
+        var isNode1SubtypeOfAttribute = node1.GetValueType().IsSubtypeOf(ValueType.Attribute);
+        var isNode2SubtypeOfAttribute = node2.GetValueType().IsSubtypeOf(ValueType.Attribute);
 
         TNode? value1;
         TNode? value2;
@@ -26,7 +61,7 @@ public class DocumentOrderUtils<TNode> where TNode : notnull
         {
             value1 = domFacade.GetParentNode(node1.Value);
             value2 = domFacade.GetParentNode(node2.Value);
-            if (value1!.Equals(value2))
+            if (value1 != null && value1.Equals(value2))
             {
                 // Same element, so A
                 return 1;
@@ -36,7 +71,7 @@ public class DocumentOrderUtils<TNode> where TNode : notnull
         {
             value1 = node1.Value;
             value2 = domFacade.GetParentNode(node2.Value);
-            if (value1!.Equals(value2))
+            if (value2 != null && value1.Equals(value2))
             {
                 // Same element, so B before A
                 return -1;
@@ -44,15 +79,20 @@ public class DocumentOrderUtils<TNode> where TNode : notnull
         }
         else if (isNode1SubtypeOfAttribute && isNode2SubtypeOfAttribute)
         {
-            if (domFacade.GetParentNode(node2.Value)!.Equals(domFacade.GetParentNode(node1.Value)))
+            var node1Parent = domFacade.GetParentNode(node1.Value);
+            var node2Parent = domFacade.GetParentNode(node2.Value);
+            if (node1Parent != null && node1Parent.Equals(node2Parent) || (node1Parent == null && node2Parent == null))
             {
                 // Sort on attributes name
-                return string.Compare(domFacade.GetLocalName(node1.Value), domFacade.GetLocalName(node2.Value),
-                    StringComparison.Ordinal);
+                return string.Compare(
+                    domFacade.GetLocalName(node1.Value),
+                    domFacade.GetLocalName(node2.Value),
+                    StringComparison.Ordinal
+                );
             }
 
-            value1 = domFacade.GetParentNode(node1.Value);
-            value2 = domFacade.GetParentNode(node2.Value);
+            value1 = node1Parent;
+            value2 = node2Parent;
         }
         else
         {
@@ -73,7 +113,7 @@ public class DocumentOrderUtils<TNode> where TNode : notnull
         var actualNodeB = nodeB;
 
         if (actualNodeA!.Equals(actualNodeB)) return 0;
-        
+
         var actualAncestorsA = FindAllAncestors(domFacade, actualNodeA);
         var actualAncestorsB = FindAllAncestors(domFacade, actualNodeB);
 
@@ -107,7 +147,7 @@ public class DocumentOrderUtils<TNode> where TNode : notnull
 
         // All nodes under a node are higher in document order than said node
         if (y >= actualAncestorsA.Count) return -1;
-        
+
         // All nodes under a node are higher in document order than said node
         if (y >= actualAncestorsB.Count) return 1;
 
