a26cec5276a41d35d774afb118a0f71265a1f20a
M.T <mtesseracttech@gmail.com>
2022-09-06 13:31:22 +0200

    Added some skeleton for NodeCompare

--- FontoXPathCSharp/Expressions/Operators/Compares/NodeCompare.cs
+++ FontoXPathCSharp/Expressions/Operators/Compares/NodeCompare.cs
@@ -1,5 +1,17 @@
+using FontoXPathCSharp.Sequences;
+
 namespace FontoXPathCSharp.Expressions.Operators.Compares;
 
-public class NodeCompare<TNode>
+public class NodeCompare<TNode> : AbstractExpression<TNode>
 {
+    //TODO: Implement Node Comparison Expression
+    public NodeCompare(Specificity specificity, AbstractExpression<TNode>[] childExpressions, OptimizationOptions optimizationOptions) : base(specificity, childExpressions, optimizationOptions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ISequence Evaluate(DynamicContext? dynamicContext, ExecutionParameters<TNode> executionParameters)
+    {
+        throw new NotImplementedException();
+    }
 }
