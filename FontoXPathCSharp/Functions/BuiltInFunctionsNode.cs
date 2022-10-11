cc6ee8953e745b0e0212e1394c2560e7fadcdac2
M.T <mtesseracttech@gmail.com>
2022-10-11 10:34:14 +0200

    Base for all missing node functions

--- FontoXPathCSharp/Functions/BuiltInFunctionsNode.cs
+++ FontoXPathCSharp/Functions/BuiltInFunctionsNode.cs
@@ -64,6 +64,54 @@ public static class BuiltInFunctionsNode<TNode>
                     FnNodeName(dynamicContext, executionParameters, staticContext, sequence));
         };
 
+    public static readonly FunctionSignature<ISequence, TNode> FnNamespaceUri =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
+    public static readonly FunctionSignature<ISequence, TNode> FnInnermost =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
+    public static readonly FunctionSignature<ISequence, TNode> FnOutermost =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
+    public static readonly FunctionSignature<ISequence, TNode> FnHasChildren =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
+    public static readonly FunctionSignature<ISequence, TNode> FnPath =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
+    public static readonly FunctionSignature<ISequence, TNode> FnLocalName =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
+    public static readonly FunctionSignature<ISequence, TNode> FnRoot =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
+    public static readonly FunctionSignature<ISequence, TNode> FnData =
+        (dynamicContext, executionParameters, staticContext, args) =>
+        {
+            return default;
+        };
+    
     public static readonly BuiltinDeclarationType<TNode>[] Declarations =
     {
         new(new[]
@@ -82,6 +130,78 @@ public static class BuiltInFunctionsNode<TNode>
             BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
             new SequenceType(ValueType.XsString, SequenceMultiplicity.ZeroOrOne)
         ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Node, SequenceMultiplicity.ExactlyOne)
+            },
+            FnNamespaceUri,
+            "namespace-uri",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsAnyUri, SequenceMultiplicity.ExactlyOne)
+        ),
+        
+        new(Array.Empty<ParameterType>(),
+            BuiltInFunctions<TNode>.ContextItemAsFirstArgument(FnNamespaceUri),
+            "namespace-uri",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsAnyUri, SequenceMultiplicity.ExactlyOne)
+        ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Node, SequenceMultiplicity.ZeroOrMore)
+            },
+            FnInnermost,
+            "innermost",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.Node, SequenceMultiplicity.ZeroOrMore)
+        ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Node, SequenceMultiplicity.ZeroOrMore)
+            },
+            FnOutermost,
+            "outermost",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.Node, SequenceMultiplicity.ZeroOrMore)
+        ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Node, SequenceMultiplicity.ZeroOrOne)
+            },
+            FnHasChildren,
+            "has-children",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsBoolean, SequenceMultiplicity.ExactlyOne)
+        ),
+        
+        new(Array.Empty<ParameterType>(),
+            BuiltInFunctions<TNode>.ContextItemAsFirstArgument(FnHasChildren),
+            "has-children",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsBoolean, SequenceMultiplicity.ExactlyOne)
+        ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Node, SequenceMultiplicity.ZeroOrOne)
+            },
+            FnPath,
+            "path",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsString, SequenceMultiplicity.ZeroOrOne)
+        ),
+        
+        new(Array.Empty<ParameterType>(),
+            BuiltInFunctions<TNode>.ContextItemAsFirstArgument(FnPath),
+            "path",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsString, SequenceMultiplicity.ZeroOrOne)
+        ),
+
 
         new(new[]
             {
@@ -98,6 +218,57 @@ public static class BuiltInFunctionsNode<TNode>
             "node-name",
             BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
             new SequenceType(ValueType.XsQName, SequenceMultiplicity.ZeroOrOne)
-        )
+        ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Node, SequenceMultiplicity.ZeroOrOne)
+            },
+            FnLocalName,
+            "local-name",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsString, SequenceMultiplicity.ExactlyOne)
+        ),
+
+        new(Array.Empty<ParameterType>(),
+            BuiltInFunctions<TNode>.ContextItemAsFirstArgument(FnLocalName),
+            "local-name",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsString, SequenceMultiplicity.ExactlyOne)
+        ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Node, SequenceMultiplicity.ZeroOrOne)
+            },
+            FnRoot,
+            "root",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.Node, SequenceMultiplicity.ZeroOrOne)
+        ),
+
+        new(Array.Empty<ParameterType>(),
+            BuiltInFunctions<TNode>.ContextItemAsFirstArgument(FnRoot),
+            "root",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.Node, SequenceMultiplicity.ZeroOrOne)
+        ),
+        
+        new(new[]
+            {
+                new ParameterType(ValueType.Item, SequenceMultiplicity.ZeroOrOne)
+            },
+            FnData,
+            "data",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsAnyAtomicType, SequenceMultiplicity.ZeroOrMore)
+        ),
+
+        new(Array.Empty<ParameterType>(),
+            BuiltInFunctions<TNode>.ContextItemAsFirstArgument(FnData),
+            "data",
+            BuiltInUri.FunctionsNamespaceUri.GetBuiltinNamespaceUri(),
+            new SequenceType(ValueType.XsAnyAtomicType, SequenceMultiplicity.ZeroOrMore)
+        ),
     };
 }
