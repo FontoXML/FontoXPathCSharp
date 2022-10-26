ac48070378127d345806eac18cb6ad4c4b95a396
M.T <mtesseracttech@gmail.com>
2022-10-26 19:50:38 +0200

    Added AnyItemType Test and fixed some small bits.

--- FontoXPathCSharp/CompileAstToExpression.cs
+++ FontoXPathCSharp/CompileAstToExpression.cs
@@ -69,10 +69,11 @@ public static class CompileAstToExpression<TNode> where TNode : notnull
     {
         return ast.Name switch
         {
-            AstNodeName.NameTest => new NameTest<TNode>(new QName(ast.TextContent)),
-            AstNodeName.AnyKindTest => CompileTypeTest(ast),
+            AstNodeName.NameTest => CompileNameTest(ast),
+            AstNodeName.AnyKindTest => CompileAnyKindTest(),
             AstNodeName.AtomicType => CompileTypeTest(ast),
             AstNodeName.AttributeTest => CompileAttributeTest(ast),
+            AstNodeName.AnyItemType => CompileAnyItemTest(),
             AstNodeName.ElementTest => CompileElementTest(ast),
             AstNodeName.CommentTest => CompileCommentTest(),
             AstNodeName.Wildcard => CompileWildcard(ast),
@@ -82,6 +83,21 @@ public static class CompileAstToExpression<TNode> where TNode : notnull
         };
     }
 
+    private static AbstractTestExpression<TNode> CompileNameTest(Ast ast)
+    {
+        return new NameTest<TNode>(ast.GetQName());
+    }
+
+    private static AbstractTestExpression<TNode> CompileAnyKindTest()
+    {
+        return new TypeTest<TNode>(new QName("node()", null, ""));
+    }
+
+    private static AbstractTestExpression<TNode> CompileAnyItemTest()
+    {
+        return new TypeTest<TNode>(new QName("item()", null, ""));
+    }
+    
     private static AbstractTestExpression<TNode> CompileCommentTest()
     {
         return new KindTest<TNode>(NodeType.Comment);
